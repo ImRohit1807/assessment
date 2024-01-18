07ed9f41aabd9acd68a2b0014178fe8951c7ae70
ImRohit1807 <59165829+ImRohit1807@users.noreply.github.com>
2024-01-18 13:12:30 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+console.log('test')
+// student , parent , teacher ,class => relationship
+// endpoints
+
+// multiple student, and teacher 
+// teacher can many class
+// parent with two kid 
+// DB 
+// teacher assigmnet to class first check present or not
+// schedule parent meeting ==>
+// assign teacher to parent 2kid having differnt time
